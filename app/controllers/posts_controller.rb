0f9a44ed7a5b1dabf2b437780eadd29bc4ea437a
Amr1192 <163312838+Amr1192@users.noreply.github.com>
2025-05-31 21:49:27 +0000

    Update Posts controller with full CRUD actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,18 +11,37 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
   def create
     @post = Post.new(post_params)
     if @post.save
       redirect_to @post
     else
-      render :new
+      render 'new'
     end
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
   end
 
   private
 
   def post_params
-    params.require(:post).permit(:title, :body)
+    params.require(:post).permit(:title, :content)
   end
 end
