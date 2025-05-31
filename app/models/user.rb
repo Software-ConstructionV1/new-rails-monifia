1bfb767d9de6c4dd412c4d03731d929ca7a9b958
ALaa-Abdullh <alaaabdullh43@gmail.com>
2025-05-31 15:36:20 +0300

    Added  User model

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+
+    validates :name, :DOB, :email, :phone_number, presence: true
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+    validates :email, uniqueness: true
+end
