4d37d74f5f7a71b8a6fbccbd66a5b6fee0413e21
Abdelrhmannali <120027987+Abdelrhmannali@users.noreply.github.com>
2025-05-31 15:49:03 +0000

    update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,24 @@
 class UsersController < ApplicationController
   def index
-    @user = "abdelrhman"
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to users_path, notice: "User created successfully"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :DOB, :email, :phone_number, :address)
   end
 end
