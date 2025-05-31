1d01f6cd0c67991d56fdac989ed7c109bd72d8b9
Ahmedhamad99 <ahaahmf123456789@gmail.com>
2025-05-31 12:30:24 +0300

    add crud for posts

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  get "/users", to: "users#index"
+  resources :posts
+
+  # get "/users", to: "users#index"
 end
