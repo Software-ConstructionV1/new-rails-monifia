a61b103e8a8b751abb2d639036fd63d63b92f663
Aya Sabry <ayasabry410@gmail.com>
2025-05-31 14:12:35 +0300

    Add CRUD for User resource

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+  validates :name, :dob, :email, :phone_number, presence: true
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
+end
