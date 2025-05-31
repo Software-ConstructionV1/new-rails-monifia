9378ff582b9e85bf2566f56aebbf2440a5a879bc
Menna Salama <mennasalama196@gmail.com>
2025-05-31 21:55:17 +0000

    Add validations to User model and unique index on email

--- db/migrate/20250531215047_create_users.rb
+++ db/migrate/20250531215047_create_users.rb
@@ -1,10 +1,12 @@
-class CreateUsers < ActiveRecord::Migration[8.0]
+class CreateUsers < ActiveRecord::Migration[7.1]
   def change
     create_table :users do |t|
       t.string :name
       t.date :dob
       t.string :email
       t.string :phone_number
+
+      t.index :email, unique: true
 
       t.timestamps
     end
