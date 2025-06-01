0b424c901ececa0262ab904c1577af5412043216
Aya Sabry <ayasabry410@gmail.com>
2025-06-01 14:35:34 +0300

    Add model associations for creator and editor

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+  belongs_to :creator, class_name: 'User'
+
+  has_many :editor_assignments
+  has_many :editors, through: :editor_assignments, source: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
   validates :name, :dob, :email, :phone_number, presence: true
+  has_many :created_posts, class_name: 'Post', foreign_key: 'creator_id'
+  has_many :editor_assignments
+  has_many :edited_posts, through: :editor_assignments, source: :post
+
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
