52fe062a1469eee84cbaaab9cc58a59b9ac0edc8
Zeyad Elmalky <81191083+zeyad19@users.noreply.github.com>
2025-06-01 12:33:07 +0000

    this is lab 4

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,17 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+users = User.create([
+  { name: "zeyad", email: "z@gmail.com" },
+  { name: "zezo", email: "ze@gmail.com" }
+])
+
+posts = Post.create([
+  { title: "Post 1", body: "Hello World by zeyad", user_id: users[0].id },
+  { title: "Post 2", body: "this is the second post", user_id: users[1].id }
+])
+
+editors = Editor.create([
+  { name: "Editor A" },
+  { name: "Editor B" }
+])
+
+posts[0].editors << editors[0]
+posts[1].editors << [editors[0], editors[1]]
