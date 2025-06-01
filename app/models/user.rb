cc0aebd191280c3d815cca4befef62a7995ab93b
YousefGamgoum <yousefsalah959@gmail.com>
2025-06-01 13:26:49 +0300

    Create users and posts with scaffold

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,2 @@
 class User < ApplicationRecord
-  validates :name, :email, :DOB, :address, :phone_number, presence: true
-  validates :email, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email address" }
 end
