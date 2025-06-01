ad99624f47306820df276e4d6957ce82092caa7d
Asmaaakhaled <asmkh8@gmail.com>
2025-06-01 15:18:17 +0300

    Setup one-to-many association between User and Post models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-    validates :name, :DOB, :email, :phone_number, presence: true
+    has_many :posts
+    validates :name, :dob, :email, :phone_number, presence: true
     VALID_EMAIL_REGEX = /\A[^@\s]+@[^@\s]+\z/
     validates :email, format: { with: VALID_EMAIL_REGEX, message: "must be a valid email" }
     validates :email, uniqueness: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_31_102639) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_01_115057) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "author"
@@ -23,6 +23,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_31_102639) do
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
